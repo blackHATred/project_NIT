0853784b5272bcbbfb4846e1ea3d2d1db1884fed
Alex <43523398+HELOPITOR@users.noreply.github.com>
2021-02-02 15:21:01 +0500

    2FA bug fix

--- controller/user/user_add_2fa.py
+++ controller/user/user_add_2fa.py
@@ -5,19 +5,26 @@ import pyotp
 import io
 
 from helpers.check_request import check_request
+from view.exceptions import InvalidUsage
 
 
 async def user_add_2fa(request: Request):
     if request.method == 'GET':
         request, user = await check_request(request, list(), True)
-        await user.add_totp()
-        img_bytes = io.BytesIO()
-        qrcode.make(
-            pyotp.totp.TOTP(user.totp_key).provisioning_uri(name=user.login, issuer_name='Napoleon IT')
-        ).save(img_bytes, format='PNG')
-        return raw(img_bytes.getvalue(), content_type='image/png')
+        if not user.totp_active:
+            await user.add_totp()
+            img_bytes = io.BytesIO()
+            qrcode.make(
+                pyotp.totp.TOTP(user.totp_key).provisioning_uri(name=user.login, issuer_name='Napoleon IT')
+            ).save(img_bytes, format='PNG')
+            return raw(img_bytes.getvalue(), content_type='image/png')
+        else:
+            raise InvalidUsage('You already added 2FA')
     else:
         request, user = await check_request(request, ['2FA_code'], True)
-        await user.check_totp(request.json.get('2FA_code'))
-        await user.update(totp_active=True)
-        return text('Success')
+        if not user.totp_active:
+            await user.check_totp(request.json.get('2FA_code'))
+            await user.update(totp_active=True)
+            return text('Success')
+        else:
+            raise InvalidUsage('You already added 2FA')
--- controller/user/user_delete_2fa.py
+++ controller/user/user_delete_2fa.py
@@ -2,11 +2,15 @@ from sanic.request import Request
 from sanic.response import text
 
 from helpers.check_request import check_request
+from view.exceptions import InvalidUsage
 
 
 async def user_delete_2fa(request: Request):
     request, user = await check_request(request, ['2FA_code'], True)
 
-    await user.check_totp(request.json.get('2FA_code'))
-    await user.update(totp_active=False, totp_key=None)
-    return text('Success')
+    if user.totp_active:
+        await user.check_totp(request.json.get('2FA_code'))
+        await user.update(totp_active=False, totp_key=None)
+        return text('Success')
+    else:
+        raise InvalidUsage('Two-factor authentication not activated')
